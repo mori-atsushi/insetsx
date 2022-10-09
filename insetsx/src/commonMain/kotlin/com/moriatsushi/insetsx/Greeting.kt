48751084054400f55f11350e31faa8b66d36544e
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-10-09 12:23:03 +0900

    Remove sample code

--- insetsx/src/commonMain/kotlin/com/moriatsushi/insetsx/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.moriatsushi.insetsx
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greeting(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
