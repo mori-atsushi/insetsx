3f7c57c0d4774de40d46c050f6f050b720a9b730
Mori-Atsushi <at.sushi.tb@gmail.com>
2022-10-09 09:18:42 +0900

    Implement ExampleApp

--- example/src/androidMain/kotlin/com/moriatsushi/insetsx/example/MainActivity.kt
+++ example/src/androidMain/kotlin/com/moriatsushi/insetsx/example/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.moriatsushi.insetsx.example
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
 
         setContent {
             ExampleApp()
--- example/src/commonMain/kotlin/com.moriatsushi.insetsx.example/ExampleApp.kt
+++ example/src/commonMain/kotlin/com.moriatsushi.insetsx.example/ExampleApp.kt
@@ -1,19 +1,94 @@
 package com.moriatsushi.insetsx.example
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.moriatsushi.insetsx.Greeting
+import androidx.compose.ui.unit.dp
+import com.moriatsushi.insetsx.safeDrawing
 
 @Composable
 fun ExampleApp() {
+    MaterialTheme {
+        Scaffold(
+            topBar = {
+                ExampleTopAppBar()
+            },
+            bottomBar = {
+                ExampleBottomAppBar()
+            }
+        ) {
+            ExampleContent(
+                modifier = Modifier.padding(it)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ExampleTopAppBar(
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        modifier = modifier
+            .background(MaterialTheme.colors.primarySurface)
+            .windowInsetsPadding(
+                WindowInsets.safeDrawing.only(WindowInsetsSides.Top + WindowInsetsSides.Horizontal)
+            ),
+        title = {
+            Text("InsetsX")
+        },
+        elevation = 0.dp
+    )
+}
+
+@Composable
+private fun ExampleBottomAppBar(
+    modifier: Modifier = Modifier
+) {
+    BottomAppBar(
+        modifier = modifier
+            .background(MaterialTheme.colors.primarySurface)
+            .windowInsetsPadding(
+                WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom + WindowInsetsSides.Horizontal)
+            ),
+        elevation = 0.dp
+    ) {
+        IconButton(onClick = { /* no op */ }) {
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = "Menu Button"
+            )
+        }
+    }
+}
+
+@Composable
+private fun ExampleContent(
+    modifier: Modifier = Modifier
+) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.windowInsetsPadding(
+            WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal)
+        ),
         contentAlignment = Alignment.Center
     ) {
-        Text(Greeting().greeting())
+        Text("Content")
     }
 }
