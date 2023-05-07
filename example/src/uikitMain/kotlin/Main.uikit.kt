4e282223f7bede1a4a48a5451411bcde7f16557d
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-05-07 15:55:18 +0900

    Refactor example

--- example/src/uikitMain/kotlin/Main.uikit.kt
+++ example/src/uikitMain/kotlin/Main.uikit.kt
@@ -1,53 +1,12 @@
+import androidx.compose.ui.main.defaultUIKitMain
 import com.moriatsushi.insetsx.WindowInsetsUIViewController
 import com.moriatsushi.insetsx.example.ExampleApp
-import kotlinx.cinterop.ObjCObjectBase
-import kotlinx.cinterop.autoreleasepool
-import kotlinx.cinterop.cstr
-import kotlinx.cinterop.memScoped
-import kotlinx.cinterop.toCValues
-import platform.Foundation.NSStringFromClass
-import platform.UIKit.UIApplication
-import platform.UIKit.UIApplicationDelegateProtocol
-import platform.UIKit.UIApplicationDelegateProtocolMeta
-import platform.UIKit.UIApplicationMain
-import platform.UIKit.UIResponder
-import platform.UIKit.UIResponderMeta
-import platform.UIKit.UIScreen
-import platform.UIKit.UIWindow
 
 fun main() {
-    val args = emptyArray<String>()
-    memScoped {
-        val argc = args.size + 1
-        val argv = (arrayOf("skikoApp") + args).map { it.cstr.ptr }.toCValues()
-        autoreleasepool {
-            UIApplicationMain(argc, argv, null, NSStringFromClass(SkikoAppDelegate))
+    defaultUIKitMain(
+        "InsetsX",
+        WindowInsetsUIViewController {
+            ExampleApp()
         }
-    }
-}
-
-class SkikoAppDelegate : UIResponder, UIApplicationDelegateProtocol {
-    companion object : UIResponderMeta(), UIApplicationDelegateProtocolMeta
-
-    @ObjCObjectBase.OverrideInit
-    constructor() : super()
-
-    private var _window: UIWindow? = null
-    override fun window() = _window
-    override fun setWindow(window: UIWindow?) {
-        _window = window
-    }
-
-    override fun application(
-        application: UIApplication,
-        didFinishLaunchingWithOptions: Map<Any?, *>?,
-    ): Boolean {
-        window = UIWindow(frame = UIScreen.mainScreen.bounds).apply {
-            rootViewController = WindowInsetsUIViewController {
-                ExampleApp()
-            }
-            makeKeyAndVisible()
-        }
-        return true
-    }
+    )
 }
